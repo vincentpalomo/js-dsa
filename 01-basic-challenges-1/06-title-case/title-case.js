680c05118845af233a8bd2d315f4860cf8714559
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 21:36:59 -0500

    completed title case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(str) {
+  // split incoming string and lowercase
+  // loop thru each word
+  // change the first index of each word to uppercase and combine the rest of the letters
+  // return all the words back to one string
+  const words = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(' ');
+}
 
 module.exports = titleCase;
