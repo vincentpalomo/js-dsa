bc4b34c13beaa62238233581900444e285f15e84
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 20:48:02 -0500

    completed get-sum and calculator challenges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,25 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+  // if op is equal to +
+  // return num1 + num2
+  // else if op is equal to -
+  // return num1 - num2
+  // else if op is equal to *
+  // return num1 • num2
+  // else if op is equal to /
+  // return num1 / num2
+  // else
+  // return 'Invalid Operator'
+  if (op == '+') {
+    return num1 + num2;
+  } else if (op == '-') {
+    return num1 - num2;
+  } else if (op == '*') {
+    return num1 * num2;
+  } else if (op == '/') {
+    return num1 / num2;
+  } else {
+    return 'Invalid Operator';
+  }
+}
 
 module.exports = calculator;
