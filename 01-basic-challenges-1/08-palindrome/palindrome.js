0bdecaf347418ef135e13321527297499b524e3c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-02 18:11:54 -0500

    completed palindrome challenge

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,35 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    // lowercase the incoming string, split spaces and remove any non alphanumeric characters
+    // create a fwd and rev pointers
+    // loop thru str
+    // check if fwd char is the same as rev char
+    // if true move fwd by 1 and rev back by 1
+    // else return false
+    // once for loop is complete return true
+    let char = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+    console.log('formatted: ', char)
+    let fcount = 0
+    let rcount = char.length - 1
+
+    // console.log('fwd:', fwd, 'rev', rev)
+    console.log('start: ', fcount, rcount)
+
+    for (let i = 0; i < str.length; i++) {
+        let fwd = char[fcount]
+        let rev = char[rcount]
+        if (fwd === rev) {
+            fcount++
+            rcount--
+            console.log(fcount, rcount)
+            console.log('true', fwd, rev)
+        } else {
+            console.log('false: ', fwd, rev)
+            return false
+        }
+    }
+
+    return true
+}
 
 module.exports = isPalindrome;
