062f933ac65af8fd33a4744e4a191028fa5bff69
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 21:07:03 -0500

    updated count occurances with one line arrow function

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,18 +1,20 @@
-function countOccurrences(str, char) {
-  // create a count variable and set to 0
-  // loop thru the string
-  // if the current index value is equal to char
-  // increment count by 1
-  // return count
-  let count = 0;
+// function countOccurrences(str, char) {
+//   // create a count variable and set to 0
+//   // loop thru the string
+//   // if the current index value is equal to char
+//   // increment count by 1
+//   // return count
+//   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === char) {
-      count++;
-    }
-  }
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
 
-  return count;
-}
+//   return count;
+// }
+
+const countOccurrences = (str, char) => str.split(char).length - 1;
 
 module.exports = countOccurrences;
