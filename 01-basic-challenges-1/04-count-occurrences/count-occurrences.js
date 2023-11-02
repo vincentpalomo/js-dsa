0f76d0406d81b8fa2f06c3db879f894cad08120a
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 20:57:21 -0500

    completed count occurances

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,18 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  // create a count variable and set to 0
+  // loop thru the string
+  // if the current index value is equal to char
+  // increment count by 1
+  // return count
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
