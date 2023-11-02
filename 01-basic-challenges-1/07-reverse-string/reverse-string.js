a39c85275125121260cf929c84c18f8e57843066
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 21:54:06 -0500

    added js built in methods

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,18 @@
-function reverseString() {}
+function reverseString(str) {
+  // create a reverse variable
+  // loop thru the str backwards
+  // add each char index back to the reverse variable
+  // return reverse
+  let reverse = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    // console.log(str[i]);
+    reverse += str[i];
+  }
+
+  return reverse;
+}
+
+// const reverseString = (str) => str.split('').reverse().join('');
 
 module.exports = reverseString;
