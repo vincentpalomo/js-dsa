9d7ef4e7d71c5918a3ea9cdd449d13a3ffbe09f8
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-01 21:18:45 -0500

    completed find max number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,19 @@
-function findMaxNumber() {}
+function findMaxNumber(num) {
+  // easy built in method return Math.max(...num)
+  // create a current number variable set to the first index in the number array
+  // loop thru the array
+  // if current number is greater than i
+  // update current number to i
+  // return current number
+  let current = num[0];
+
+  for (let i = 1; i < num.length; i++) {
+    if (current < num[i]) {
+      current = num[i];
+    }
+  }
+
+  return current;
+}
 
 module.exports = findMaxNumber;
