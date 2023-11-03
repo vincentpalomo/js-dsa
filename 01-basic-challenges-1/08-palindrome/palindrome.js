536d7581c80e5e2d294f8da64f75752bc6376bf3
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-02 19:09:32 -0500

    completed counting vowels challenge

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -8,12 +8,12 @@ function isPalindrome(str) {
   // once for loop is complete return true
   let char = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-  console.log('formatted: ', char);
+  // console.log('formatted: ', char);
   let fcount = 0;
   let rcount = char.length - 1;
 
   // console.log('fwd:', fwd, 'rev', rev)
-  console.log('start: ', fcount, rcount);
+  // console.log('start: ', fcount, rcount);
 
   for (let i = 0; i < str.length; i++) {
     let fwd = char[fcount];
@@ -21,10 +21,10 @@ function isPalindrome(str) {
     if (fwd === rev) {
       fcount++;
       rcount--;
-      console.log(fcount, rcount);
-      console.log('true', fwd, rev);
+      // console.log(fcount, rcount);
+      // console.log('true', fwd, rev);
     } else {
-      console.log('false: ', fwd, rev);
+      // console.log('false: ', fwd, rev);
       return false;
     }
   }
@@ -32,50 +32,50 @@ function isPalindrome(str) {
   return true;
 }
 
-function isPalindromeEZ(str) {
-  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  const reversedStr = formattedStr.split('').reverse().join('');
-  return formattedStr === reversedStr;
-}
+// function isPalindromeEZ(str) {
+//   const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//   const reversedStr = formattedStr.split('').reverse().join('');
+//   return formattedStr === reversedStr;
+// }
 
-console.log('is a palindrome EZ? ', isPalindromeEZ('madam'));
+// console.log('is a palindrome EZ? ', isPalindromeEZ('madam'));
 
-function isPalindromeASCII(str) {
-  const formattedStr = removeNonAlphaNumeric(str.toLowerCase());
-  const reversedStr = reversedString(formattedStr);
-  return formattedStr === reversedStr;
-}
+// function isPalindromeASCII(str) {
+//   const formattedStr = removeNonAlphaNumeric(str.toLowerCase());
+//   const reversedStr = reversedString(formattedStr);
+//   return formattedStr === reversedStr;
+// }
 
-function removeNonAlphaNumeric(str) {
-  let formattedStr = '';
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (isAlphaNumeric(char)) {
-      formattedStr += char;
-    }
-  }
-  return formattedStr;
-}
+// function removeNonAlphaNumeric(str) {
+//   let formattedStr = '';
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+//     if (isAlphaNumeric(char)) {
+//       formattedStr += char;
+//     }
+//   }
+//   return formattedStr;
+// }
 
-function isAlphaNumeric(char) {
-  const code = char.charCodeAt(0);
-  return (
-    (code >= 48 && code <= 57) || // Numbers 0-9
-    (code >= 97 && code <= 122) // Lowercase a-z
-  );
-}
+// function isAlphaNumeric(char) {
+//   const code = char.charCodeAt(0);
+//   return (
+//     (code >= 48 && code <= 57) || // Numbers 0-9
+//     (code >= 97 && code <= 122) // Lowercase a-z
+//   );
+// }
 
-function reversedString(str) {
-  let reversed = '';
+// function reversedString(str) {
+//   let reversed = '';
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
 
-  return reversed;
-}
+//   return reversed;
+// }
 
-console.log('is a palindrome ASCII? ', isPalindromeASCII('hello!!!AAA'));
-console.log('is a palindrome ASCII? ', isPalindromeASCII('racecar'));
+// console.log('is a palindrome ASCII? ', isPalindromeASCII('hello!!!AAA'));
+// console.log('is a palindrome ASCII? ', isPalindromeASCII('racecar'));
 
 module.exports = isPalindrome;
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,25 @@
-function countVowels() {}
+function countVowels(str) {
+  // start count variable at 0
+  // create a for loop and run thru the str
+  // check each letter if they are [a e i o u]
+  // if true increase count by 1
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'a' || str[i] === 'A') {
+      count++;
+    } else if (str[i] === 'e' || str[i] === 'E') {
+      count++;
+    } else if (str[i] === 'i' || str[i] === 'I') {
+      count++;
+    } else if (str[i] === 'o' || str[i] === 'O') {
+      count++;
+    } else if (str[i] === 'u' || str[i] === 'U') {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countVowels;
