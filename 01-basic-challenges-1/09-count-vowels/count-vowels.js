08c41013649abe83aebe4f6f3a043f6f48528f85
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-02 19:14:04 -0500

    implemented countVowel with making input str lowercase

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -5,19 +5,29 @@ function countVowels(str) {
   // if true increase count by 1
   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === 'a' || str[i] === 'A') {
-      count++;
-    } else if (str[i] === 'e' || str[i] === 'E') {
-      count++;
-    } else if (str[i] === 'i' || str[i] === 'I') {
-      count++;
-    } else if (str[i] === 'o' || str[i] === 'O') {
-      count++;
-    } else if (str[i] === 'u' || str[i] === 'U') {
+  let formattedStr = str.toLowerCase();
+  //   console.log(formattedStr);
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr[i];
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
       count++;
     }
   }
+
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (str[i] === 'a' || str[i] === 'A') {
+  //       count++;
+  //     } else if (str[i] === 'e' || str[i] === 'E') {
+  //       count++;
+  //     } else if (str[i] === 'i' || str[i] === 'I') {
+  //       count++;
+  //     } else if (str[i] === 'o' || str[i] === 'O') {
+  //       count++;
+  //     } else if (str[i] === 'u' || str[i] === 'U') {
+  //       count++;
+  //     }
+  //   }
 
   return count;
 }
