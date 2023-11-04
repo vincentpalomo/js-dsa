68b4fb724c8687c40f632939d18f447c09e25485
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-03 20:08:50 -0500

    completed are all chars unique using set

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  if (str === '' || str.lenght === 1) return true;
+  let seen = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (!seen.has(str[i])) {
+      seen.add(str[i]);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
