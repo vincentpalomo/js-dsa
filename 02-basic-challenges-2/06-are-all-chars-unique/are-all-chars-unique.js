0af19d0095b3354bd52c0855fecd16c2e47ef36b
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-03 20:18:45 -0500

    updated and completed are all chars unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,12 +1,24 @@
 function areAllCharactersUnique(str) {
   if (str === '' || str.lenght === 1) return true;
-  let seen = new Set();
+  //   let seen = new Set();
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (!seen.has(str[i])) {
+  //       seen.add(str[i]);
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+
+  let charCount = {}; // objects need a value and a key
+
   for (let i = 0; i < str.length; i++) {
-    if (!seen.has(str[i])) {
-      seen.add(str[i]);
-    } else {
+    let char = str[i];
+    if (charCount[char]) {
       return false;
+    } else {
+      charCount[char] = true;
     }
+    // console.log(charCount);
   }
 
   return true;
