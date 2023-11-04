19c271364d8a86b08c137215e238b2a3ded10aa2
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-03 19:47:06 -0500

    completed find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,30 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  // create an alphabet string
+  // create a starter pointer at 0
+  // loop thru alphabet
+  //   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  //   const start = alphabet.indexOf(arr[0]);
+
+  //   for (let i = 0; i < arr.length; i++) {
+  //     // console.log(alphabet[start + i]);
+  //     if (arr[i] != alphabet[start + i]) {
+  //       return alphabet[start + i];
+  //     }
+  //   }
+
+  let start = arr[0].charCodeAt(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i].charCodeAt(0);
+
+    if (current - start > 1) {
+      return String.fromCharCode(start + 1);
+    }
+
+    start = current;
+  }
+
+  return '';
+}
 
 module.exports = findMissingLetter;
