153002b9cdc3dce5cae49eb4e2db1914f75ff664
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 10:06:53 -0600

    solved calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,15 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  //loop thru products array
+  // map price * quantity
+  // reduce total and sum
+  // return the high order method array * tax
+
+  const sale = products.map((item) => item.price * item.quantity).reduce((total, sum) => total + sum, 0);
+  // console.log(sale, tax);
+
+  const taxAmount = (sale * tax) / 100;
+
+  return sale + taxAmount;
+}
 
 module.exports = calculateTotalSalesWithTax;
