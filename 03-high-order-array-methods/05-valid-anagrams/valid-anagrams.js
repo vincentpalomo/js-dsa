b9075999ba3d670ab33bbe8adbcac859ba3882b6
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 15:32:51 -0600

    able to implement freq count

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -24,6 +24,18 @@ function validAnagrams(str1, str2) {
 
   //   return word1 === word2;
 
+  // const freqCount1 = str1.split('').reduce((acc, char) => {
+  //   acc[char] = (acc[char] || 0) + 1;
+  //   return acc;
+  // }, {});
+
+  // const freqCount2 = str2.split('').reduce((acc, char) => {
+  //   acc[char] = (acc[char] || 0) + 1;
+  //   return acc;
+  // }, {});
+
+  // return Object.keys(freqCount1).every((char) => freqCount1[char] === freqCount2[char]);
+
   const freqCount1 = str1.split('').reduce((acc, char) => {
     acc[char] = (acc[char] || 0) + 1;
     return acc;
