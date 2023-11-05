0c9b34c4ec9c788b39efc4c97681e2dce332da73
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-04 19:04:58 -0500

    implemented regular expressions for solving email validation

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -14,24 +14,29 @@
 //   }
 // }
 
+// function validateEmail(str) {
+//   if (str.indexOf('@') === -1) {
+//     return false;
+//   }
+
+//   const [local, domain] = str.split('@');
+
+//   if (local.length === 0 || domain.length < 3) {
+//     return false;
+//   }
+
+//   const domainExtension = domain.split('.');
+
+//   if (domainExtension.length < 2 || domainExtension[1] < 2) {
+//     return false;
+//   }
+
+//   return true;
+// }
+
 function validateEmail(str) {
-  if (str.indexOf('@') === -1) {
-    return false;
-  }
-
-  const [local, domain] = str.split('@');
-
-  if (local.length === 0 || domain.length < 3) {
-    return false;
-  }
-
-  const domainExtension = domain.split('.');
-
-  if (domainExtension.length < 2 || domainExtension[1] < 2) {
-    return false;
-  }
-
-  return true;
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  return emailRegex.test(str);
 }
 
 module.exports = validateEmail;
