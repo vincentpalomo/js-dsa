3996de5ded40eb03b27552b6baa5ff9aaf79bb97
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 10:16:40 -0600

    could solve the calculate total sales with just using reduce

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -4,8 +4,10 @@ function calculateTotalSalesWithTax(products, tax) {
   // reduce total and sum
   // return the high order method array * tax
 
-  const sale = products.map((item) => item.price * item.quantity).reduce((total, sum) => total + sum, 0);
+  // const sale = products.map((item) => item.price * item.quantity).reduce((total, sum) => total + sum, 0);
   // console.log(sale, tax);
+
+  const sale = products.reduce((total, product) => total + product.price * product.quantity, 0);
 
   const taxAmount = (sale * tax) / 100;
 
