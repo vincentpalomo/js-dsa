b806dc44a7e6e57628dd28b31fde1227ab79163e
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 16:37:19 -0600

    errors with valid ip

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,22 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (add) => {
+  // split input string by .
+  // if split length is less than 4 or greater than 4 return false
+  // map thru split to check each number inside
+  // if the values are not between 0 - 255 return false
+  // if first value of the number is 088 return false
+
+  const ipNums = add.split('.').map((numbers) => {
+    if (numbers < 0 || numbers > 255) {
+      return false;
+    }
+    return numbers;
+  });
+
+  if (ipNums.length > 4 || ipNums.length < 4) {
+    return false;
+  }
+
+  return true;
+};
 
 module.exports = isValidIPv4;
