25b3440f623f98d2820f8cfe6a27164a7849c457
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 09:24:56 -0600

    solved sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,18 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  // if array lenght is 0 return 0
+  // filter out the even numbers using % 2
+  // use map to square each number from the new filtered array
+  // use reduce to add up the sum of the new mapped array
+
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const squaredNumbers = evenNumbers.map((num) => num * num);
+  const sumNumber = squaredNumbers.reduce((total, num) => total + num, 0);
+
+  return sumNumber;
+}
 
 module.exports = sumOfEvenSquares;
