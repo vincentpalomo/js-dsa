92e02d1c507ad36fa173a77f1d6386854a59d8c6
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-04 19:03:58 -0500

    implemented validate email using indexOf and split

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,17 +1,37 @@
+// function validateEmail(str) {
+//   // put string into a new Set
+//   // check if set if includes @ and .
+//   // if true return true
+//   // else return false
+
+//   let isValid = new Set(str);
+//   //   console.log(isValid);
+
+//   if (isValid.has('@') && isValid.has('.') && str[0] != '@') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
 function validateEmail(str) {
-  // put string into a new Set
-  // check if set if includes @ and .
-  // if true return true
-  // else return false
-
-  let isValid = new Set(str);
-  console.log(isValid);
-
-  if (isValid.has('@') && isValid.has('.') && str[0] != '@') {
-    return true;
-  } else {
+  if (str.indexOf('@') === -1) {
     return false;
   }
+
+  const [local, domain] = str.split('@');
+
+  if (local.length === 0 || domain.length < 3) {
+    return false;
+  }
+
+  const domainExtension = domain.split('.');
+
+  if (domainExtension.length < 2 || domainExtension[1] < 2) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = validateEmail;
