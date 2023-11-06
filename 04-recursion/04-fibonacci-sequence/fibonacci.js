6f1c981585dd58ed356d55d5b107428c1a32b8a7
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 17:27:15 -0600

    solved fibonacci using recursive

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,15 @@
-function fibonacci() {}
+function fibonacci(num) {
+  // base case
+  if (num < 2) {
+    return num;
+  }
+  // recursive case
+  const call1 = fibonacci(num - 1);
+  const call2 = fibonacci(num - 2);
+  //   console.log(call1, call2);
+  const result = call1 + call2;
+  //   console.log(result);
+  return result;
+}
 
 module.exports = fibonacci;
