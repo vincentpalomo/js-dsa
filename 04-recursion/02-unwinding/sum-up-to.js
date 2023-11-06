d1fd71a246e4eae4cea53932ce8db2f39e30a859
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 16:31:41 -0600

    completed unwinding sum up to and reverse string recursive practice

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,15 @@
-function sumUpTo() {}
+function sumUpTo(n) {
+  // base case
+  if (n === 1) {
+    // console.log(`sumUpTo(${n}) returns 1`);
+    return 1;
+  }
+
+  // recursive case
+  // console.log(`sumUpTo(${n}) calls sumUpTo(${n - 1}) + ${n}`);
+  const result = n + sumUpTo(n - 1);
+  // console.log(`sumUpTo(${n}) returns ${result}`);
+  return result;
+}
 
 module.exports = sumUpTo;
--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,21 @@
-function reverseString() {}
+function reverseString(str) {
+  // base case
+  // if string is empty '' return ''
+  if (str === '') {
+    // console.log('base case reached: ', str);
+    return '';
+  }
+
+  // recursive case
+  // call the function and start from the second character then adding the first to the end
+  //   console.log('before recursive call: ', str.substring(1) + str[0]);
+  const result = reverseString(str.substring(1)) + str[0];
+  //   console.log('after recursive call: ', str.substring(1) + str[0]);
+  return result;
+}
+
+// const reverseStringAF = (str) => (str === '' ? '' : reverseStringAF(str.substring(1)) + str.charAt(0));
+// const result = reverseStringAF('jinx');
+// console.log(result);
 
 module.exports = reverseString;
