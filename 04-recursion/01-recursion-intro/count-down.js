0e269511ccaf7199646839d625c105fd1ca469b7
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 15:50:08 -0600

    completed basic recursion example

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,14 @@
-function countDown() {}
+function countDown(num) {
+  // base case
+  if (num <= 0) {
+    console.log('All done!');
+    return;
+  }
+
+  // recursive case
+  console.log(num);
+  num--;
+  countDown(num);
+}
 
 module.exports = countDown;
