55f959c43cc9e2a7cf4b7397978a79108a6d31b0
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-05 19:53:44 -0600

    updated

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -6,6 +6,17 @@ const isValidIPv4 = (add) => {
   // check if the number is between 0 and 255
   // check if the octet is equal to the number converted back to a string, this is to check for leading 0
 
+  // const numbers = add.split('.');
+
+  // if (numbers.length !== 4) {
+  //   return false;
+  // }
+
+  // return numbers.every((octet) => {
+  //   const num = parseInt(octet);
+  //   return num >= 0 && num <= 255 && octet === num.toString();
+  // });
+
   const numbers = add.split('.');
 
   if (numbers.length !== 4) {
@@ -13,7 +24,7 @@ const isValidIPv4 = (add) => {
   }
 
   return numbers.every((octet) => {
-    const num = parseInt(octet);
+    let num = parseInt(octet);
     return num >= 0 && num <= 255 && octet === num.toString();
   });
 };
