06ede5100b7d4a0711259898559296eda002f454
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 15:18:14 -0600

    errors in current logic

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -5,3 +5,24 @@ Logarithmic time means that the time required to complete a function is proporti
 
 */
 
+function findPower(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+
+  if (exponent % 2 === 0) {
+    const halfPower = findPower(base, exponent / 2);
+    return halfPower * halfPower;
+  } else {
+    const halfPower = findPower(base, (exponent - 1) / 2);
+    return base * halfPower * halfPower;
+  }
+}
+
+console.time('Find Power 1');
+findPower(2, 100);
+console.timeEnd('Find Power 1');
+
+console.time('Find Power 2');
+findPower(2, 1000000000);
+console.timeEnd('Find Power 2');
--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,23 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let total = 0;
+  let highest = 0;
+  // console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    console.log('current number: ', current);
+    for (let j = i; j < k; j++) {
+      console.log('next k: ', arr[j]);
+      total += arr[j];
+      console.log('total: ', total);
+    }
+    if (total > highest) {
+      highest = total;
+      console.log('highest: ', highest);
+      total = 0;
+    }
+  }
+
+  console.log('highest outside: ', highest);
+}
 
 module.exports = maxSubarraySum;
