b4fb991aad6eba8f07544f0aa31f73e4849c182f
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 12:30:58 -0600

    added comments to flatten array challenge

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,8 +1,16 @@
 function flattenArray(arr) {
-  let result = [];
+  // create an empty result array
+  // loop thru the arr using for...of loop
+  // if the current element is an array, recursively call flattenArray to flatten further
+  // then concatenate the result to the result array
+  // if the current element is not an array, push it into the result array
+  // return result
 
+  let result = [];
+  // base case
   for (const item of arr) {
     if (Array.isArray(item)) {
+      // recursive case
       result = result.concat(flattenArray(item));
     } else {
       result.push(item);
