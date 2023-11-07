98b164262cbd9394335cebf364629205a297eb3a
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 19:30:13 -0600

    solved array sum recursive

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,16 @@
-function arraySum() {}
+function arraySum(arr) {
+  // console.log(arr);
+  // base case
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  // recursive case
+  // let sum = 0;
+  // sum += arr[0];
+  // const result = arraySum(arr.slice(1));
+  // sum += result;
+  return (arr[0] += arraySum(arr.slice(1)));
+}
 
 module.exports = arraySum;
