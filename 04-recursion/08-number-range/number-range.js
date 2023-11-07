77acab622386bf1debc399608d2d98ffd9a84ad3
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 21:49:26 -0600

    solved number range

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,13 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  // base case
+  if (startNum === endNum) {
+    return [startNum];
+  }
+  // recursive case
+  let numbers = [];
+  numbers.push(startNum);
+  const result = numberRange(startNum + 1, endNum);
+  return (numbers = numbers.concat(result));
+}
 
 module.exports = numberRange;
