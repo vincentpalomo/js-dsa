cd86e8337e56976824d4349798bb6e6c0c95f2c0
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 12:46:48 -0600

    completed permutations using recursion

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,23 @@
-function permutations() {}
+function permutations(str) {
+  let result = [];
+  // base case
+  if (str.length === 0) {
+    result.push('');
+    return result;
+  }
+
+  // recursive case
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restOfString = str.slice(0, i) + str.slice(i + 1);
+    const subPermutations = permutations(restOfString);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(firstChar + subPermutations[j]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = permutations;
