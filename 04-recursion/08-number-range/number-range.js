9bfb2684357fc80aead8ea38e7f0a6344434eef6
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 21:53:53 -0600

    update solution provided by brad

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -4,10 +4,13 @@ function numberRange(startNum, endNum) {
     return [startNum];
   }
   // recursive case
-  let numbers = [];
-  numbers.push(startNum);
-  const result = numberRange(startNum + 1, endNum);
-  return (numbers = numbers.concat(result));
+  // let numbers = [];
+  // numbers.push(startNum);
+  // const result = numberRange(startNum + 1, endNum);
+  // return (numbers = numbers.concat(result));
+  const numbers = numberRange(startNum, endNum - 1);
+  numbers.push(endNum);
+  return numbers;
 }
 
 module.exports = numberRange;
