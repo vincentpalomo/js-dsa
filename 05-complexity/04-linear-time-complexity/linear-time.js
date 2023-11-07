138cd467c934cbabc7fbc737c052856969beaadc
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 14:31:51 -0600

    learned about linear time big O

--- 05-complexity/04-linear-time-complexity/linear-time.js
+++ 05-complexity/04-linear-time-complexity/linear-time.js
@@ -16,3 +16,8 @@ const arr1 = [1, 2, 3, 4, 5];
 console.time('Sum Array 1');
 console.log(sumArray(arr1));
 console.timeEnd('Sum Array 1');
+
+const arr2 = Array.from({ length: 10000000 }, (_, index) => index + 1);
+console.time('Sum Array 2');
+console.log(sumArray(arr2));
+console.timeEnd('Sum Array 2');
