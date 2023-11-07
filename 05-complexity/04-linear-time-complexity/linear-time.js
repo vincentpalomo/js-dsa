0e0b4fb0935853dff1a84c364374e04c2d25d09a
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 13:06:41 -0600

    continue learning in bedroom

--- 05-complexity/04-linear-time-complexity/linear-time.js
+++ 05-complexity/04-linear-time-complexity/linear-time.js
@@ -3,3 +3,16 @@
 
   Linear time means that the time required to complete a function is directly proportional to the size of the input data set.
 */
+
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+const arr1 = [1, 2, 3, 4, 5];
+console.time('Sum Array 1');
+console.log(sumArray(arr1));
+console.timeEnd('Sum Array 1');
