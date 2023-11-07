1d5c575f11dad60cd9425aa6a1692a258f94e5e9
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-06 19:04:20 -0600

    solved power recursive

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,14 +1,14 @@
 function factorial(num) {
-  console.log(`Calculating factorial(${num})`);
+  // console.log(`Calculating factorial(${num})`);
   // base case
   if (num === 0 || num === 1) {
-    console.log(`Base case reached for factorial(${num})`);
+    // console.log(`Base case reached for factorial(${num})`);
     return 1;
   }
   // recursive case
   // return num * factorial(num - 1);
   const result = num * factorial(num - 1);
-  console.log(`factorial(${num}) = ${num} * factorial(${num - 1}) = ${num} * factorial(${num - 1}) = ${result}`);
+  // console.log(`factorial(${num}) = ${num} * factorial(${num - 1}) = ${num} * factorial(${num - 1}) = ${result}`);
   return result;
 }
 
--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,7 +1,11 @@
 function power(num, pwr) {
   // base case
   // if num power 0 return 1
+  if (pwr === 0) {
+    return 1;
+  }
   // recursive case
+  return num * power(num, pwr - 1);
 }
 
 module.exports = power;
