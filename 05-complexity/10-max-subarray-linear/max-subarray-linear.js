b8657c006d5e5e16a2a82b50ec3d20832b9512ab
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 18:58:50 -0600

    added code to see how the window moves

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,32 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  // create a variable for current sum
+  // create a variable for max sum
+  // get the sum of the first window 'k'
+  // set the current sum to max sum
+  // use the sliding window approach to find the max subarray sum
+  // update the current sum by removing the leftmost element of the window
+  // add the rightmost element of the window
+  // update the max sum if the current sum is greater
+  // return max sum
+
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+
+  currentSum = maxSum;
+
+  for (let i = k; i < arr.length; i++) {
+    console.log('window: ', [arr[i - 2], arr[i - 1], arr[i]], '-> next window: ', [arr[i - 1], arr[i], arr[i + 1]]);
+    currentSum = currentSum - arr[i - k] + arr[i];
+    maxSum = Math.max(maxSum, currentSum);
+    console.log('remove left: ', arr[i - k], 'add right: ', arr[i], 'currentSum: ', currentSum, 'maxSum: ', maxSum);
+    console.log('');
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
--- 05-complexity/10-max-subarray-linear/readme.md
+++ 05-complexity/10-max-subarray-linear/readme.md
@@ -25,7 +25,7 @@ console.log(maxSubarraySum(arr1, k1)); // Output: 24
 
 const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4];
 const k2 = 4;
-console.log(maxSubarraySum(arr2, k2)); // Output: -9
+console.log(maxSubarraySum(arr2, k2)); // Output: -11
 ```
 
 ### Constraints
@@ -92,7 +92,7 @@ test('Finding maximum subarray sum using O(n^2) solution', () => {
 
   const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4];
   const k2 = 4;
-  expect(maxSubarraySum(arr2, k2)).toBe(-9);
+  expect(maxSubarraySum(arr2, k2)).toBe(-11);
 });
 ```
 
