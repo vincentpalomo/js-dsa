ad2d4c5153f3c624acbff89f8f26bdec5aa14ce0
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-07 21:54:48 -0600

    solved word frequency counter

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,41 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  // split the input string, lowercase and remove any punctuation marks
+  // create an empty map to track each word
+  // loop thru the split string
+  // if word is in the map add one to the value
+  // else add word and set value to 1
+
+  const freqCount = new Map();
+
+  const words = str.toLowerCase().split(/\W+/);
+
+  // const words = str
+  //   .toLowerCase()
+  //   .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+  //   .split(' ');
+  // console.log(words);
+
+  // for (let i = 0; i < words.length; i++) {
+  //   if (freqCount.has(words[i])) {
+  //     console.log('true', words[i]);
+  //     freqCount.set(words[i], freqCount.get(words[i]) + 1);
+  //   } else {
+  //     console.log('false', words[i]);
+  //     freqCount.set(words[i], 1);
+  //   }
+  // }
+
+  for (const word of words) {
+    if (word === '') continue;
+
+    if (freqCount.has(word)) {
+      freqCount.set(word, freqCount.get(word) + 1);
+    } else {
+      freqCount.set(word, 1);
+    }
+  }
+
+  return freqCount;
+}
 
 module.exports = wordFrequencyCounter;
