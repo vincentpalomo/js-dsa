a9f9c2740559ba372f315aaad3609dc03c9e0079
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-08 19:15:43 -0600

    solved phone number directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,18 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+  // Create a new Map called directory
+  // Iterate through the phoneNumbers array using a for...of loop
+  // Use the split() method to separate the name and phone number from each entry using the colon : as the separator
+  // Set each name as the key and its corresponding phone number as the value in the Map
+  // Return the directory, which now contains the phone number directory
+
+  const directory = new Map();
+
+  for (const entry of phoneNumbers) {
+    const [name, number] = entry.split(':');
+    directory.set(name, number);
+  }
+
+  return directory;
+}
 
 module.exports = phoneNumberDirectory;
---
+++ 06-hash-tables-maps-sets/05-phone-number-directory/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(result.get('John'));
