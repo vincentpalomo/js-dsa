0f032c6b5442785a259f886e518cef6b315bb6de
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-08 19:43:01 -0600

    solved anagram grouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,25 @@
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  console.log(words);
+  // create a new map variable
+  // loop thru the words array
+  // sort the word
+  // check if the current word is in the new map
+  // if not add the word to the map key will be the sorted word and the value will be an array of the word
+  // else add the word to the value array
+  // return the map back to an array using Array.from()
+
+  let anagrams = new Map();
+
+  for (const word of words) {
+    const sorted = word.split('').sort().join('');
+    if (!anagrams.has(sorted)) {
+      anagrams.set(sorted, [word]);
+    } else {
+      anagrams.get(sorted).push(word);
+    }
+  }
+
+  return Array.from(anagrams.values());
+}
 
 module.exports = anagramGrouping;
