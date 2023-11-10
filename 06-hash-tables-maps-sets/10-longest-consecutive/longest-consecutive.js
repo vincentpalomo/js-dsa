bc70c955ea4da2f6ea6924cec48009a50c27831c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-09 18:37:04 -0600

    solved longest consecutive

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,27 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+  // create set
+  // create variable for longestSequence
+  // loop thru numset and check if numset does not have num - 1
+
+  const numSet = new Set(nums);
+
+  let longestSequence = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentSequence = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentSequence++;
+      }
+
+      longestSequence = Math.max(longestSequence, currentSequence);
+    }
+  }
+
+  return longestSequence;
+}
 
 module.exports = longestConsecutiveSequence;
