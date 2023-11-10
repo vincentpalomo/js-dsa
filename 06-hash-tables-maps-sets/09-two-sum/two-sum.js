d2f3df904fd773000b99da133a2af2ae3048dce9
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-09 18:26:09 -0600

    solved two-sum using sets

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,25 @@
-function twoSum() {}
+function twoSum(nums, target) {
+  // create set
+  // iterate through input array
+  // create a compliment inside the loop
+  // check if compliment is inside the set
+  // return the index of the compliment and current index
+  // add current number into set
+  // if nothing is found return an empty array
+
+  const numSet = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    const compliment = target - nums[i];
+
+    if (numSet.has(compliment)) {
+      return [nums.indexOf(compliment), i];
+    }
+
+    numSet.add(nums[i]);
+  }
+
+  return [];
+}
 
 module.exports = twoSum;
