fdec86f1b18698a73c10a66163d75c26de52ce84
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-09 19:32:19 -0600

    completed hash tables maps and sets

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,19 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  let anagrams = new HashTable();
+
+  for (const word of words) {
+    const sorted = word.split('').sort().join('');
+
+    if (!anagrams.has(sorted)) {
+      anagrams.set(sorted, [word]);
+    } else {
+      anagrams.get(sorted).push(word);
+    }
+  }
+
+  return Array.from(anagrams.getValues());
+}
 
 module.exports = anagramGrouping;
