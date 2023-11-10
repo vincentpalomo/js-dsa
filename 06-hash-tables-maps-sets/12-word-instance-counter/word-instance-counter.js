280a9f48f77309b0c698d4000e196b57bae5e7de
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-09 19:25:39 -0600

    solved word instance counter

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,25 @@
 const HashTable = require('./HashTable');
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+  const freqCount = new HashTable();
+
+  const words = str.toLowerCase().split(/\W+/);
+
+  const targetWord = word.toLowerCase();
+
+  for (const word of words) {
+    if (word === '') continue;
+
+    if (freqCount.has(word)) {
+      freqCount.set(word, freqCount.get(word) + 1);
+    } else {
+      freqCount.set(word, 1);
+    }
+  }
+
+  if (freqCount.has(targetWord)) {
+    return freqCount.get(targetWord);
+  }
+}
 
 module.exports = wordInstanceCounter;
