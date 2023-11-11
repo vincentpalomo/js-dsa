c223a8226e7d55c1840a16722ddc156a032cc9d5
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-10 18:12:39 -0600

    reversed string using stack working

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,30 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  // create an empty stack
+  // create a reverse empty string
+  // loop thru the str
+  // push the string into the stack
+  // pop each element out of the stack
+  // return reverse str
+
+  const stack = new Stack();
+  let reversed = '';
+
+  for (const char of str) {
+    stack.push(char);
+  }
+
+  //   for (let i = 0; i < str.length; i++) {
+  //     let charPop = stack.pop();
+  //     reversed = reversed + charPop;
+  //   }
+
+  while (!stack.isEmpty()) {
+    reversed += stack.pop();
+  }
+
+  return reversed;
+}
 
 module.exports = reverseStringStack;
