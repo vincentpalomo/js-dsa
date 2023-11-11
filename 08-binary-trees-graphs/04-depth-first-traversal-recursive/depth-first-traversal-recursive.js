c3f91ed2de6fdb38525ce14710700ed6f06a0352
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 13:46:44 -0600

    depth first traversal using recursion solved

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,6 +1,27 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function recDepthFirstTraversal() {}
+function recDepthFirstTraversal(root) {
+  const result = [];
+
+  function traverse(node) {
+    if (node !== null) {
+      result.push(node.data);
+      // recursive calls
+      traverse(node.left);
+      traverse(node.right);
+    }
+  }
+
+  traverse(root);
+
+  return result;
+}
 
 module.exports = {
   Node,
