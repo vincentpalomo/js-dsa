fe1559b413f0fe85a8c5e018a743b3af4e180b6e
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 15:52:18 -0600

    implemented maxDepth algorithm

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,39 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+  // if (!root) return 0;
+
+  // const stack = [{ node: root, depth: 1 }];
+
+  // let maxDepth = 0;
+
+  // while (stack.length > 0) {
+  //   const { node, depth } = stack.pop();
+
+  //   if (depth > maxDepth) {
+  //     maxDepth = depth;
+  //   }
+
+  //   if (node.right) stack.push({ node: node.right, depth: depth + 1 });
+
+  //   if (node.left) stack.push({ node: node.left, depth: depth + 1 });
+  // }
+
+  // return maxDepth;
+
+  if (!root) return [];
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+}
 
 module.exports = {
   maxDepth,
