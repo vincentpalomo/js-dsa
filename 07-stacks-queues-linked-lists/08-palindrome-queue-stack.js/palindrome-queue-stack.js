fc2c4fc68e176d6741db9f42c51923c04bbfe4de
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-10 22:20:17 -0600

    solved palindrome queue stack

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,34 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+  // create a new queue first in first out can be for the string going forward
+  // create a new stack last in first out can be used to reverse
+  // remove all non alphanumberic characters from the string
+  // loop thru input str maybe can use for of
+  // add each char to queue and stack
+  // if queue element === to stack element
+  // return true
+  // else false
+
+  const forward = new Queue();
+  const reversed = new Stack();
+
+  const formattedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const element = formattedStr.charAt(i);
+    forward.enqueue(element);
+    reversed.push(element);
+  }
+
+  while (!forward.isEmpty()) {
+    if (forward.dequeue() !== reversed.pop()) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = isPalindromeQueueStack;
