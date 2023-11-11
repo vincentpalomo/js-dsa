504cf68d772b5631a697af86077fb6d2398bd71c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-10 18:49:11 -0600

    solved balanced parenthesis

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,28 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+  // create an empty opening parenthesis ( stack
+  // loop thru str
+  // if closing parenthesis ) is encountered pop stack
+  // if stack is empty and encountering a closing parenthesis ) return false
+  // return true
+
+  const openParenthesis = new Stack();
+
+  for (const parenthesis of str) {
+    if (parenthesis === '(') {
+      openParenthesis.push(parenthesis);
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ')') {
+      if (openParenthesis.isEmpty()) return false;
+      openParenthesis.pop();
+    }
+  }
+
+  return openParenthesis.isEmpty();
+}
 
 module.exports = balancedParenthesis;
