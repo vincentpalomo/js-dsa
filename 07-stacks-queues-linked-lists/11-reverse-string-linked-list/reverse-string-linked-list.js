cb1fefe8504dba574c21df6335913906b4ea5c9c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 09:01:16 -0600

    reversed string using linkedlist

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,27 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  // create a char placeholder LinkedList
+  // create a reversed empty string
+  // loop thru the string add each element to the linked list
+  // loop thru the linked list and build the reverse string
+
+  const list = new LinkedList();
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str.charAt(i);
+    list.add(char);
+  }
+
+  let current = list.head;
+
+  while (current !== null) {
+    reversed += current.data;
+    current = current.next;
+  }
+
+  return reversed;
+}
 
 module.exports = reverseStringLinkedList;
