5de74a3e0b7ace83a442eaea5ff848c95a462b27
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 08:43:24 -0600

    implementing linked list in javascript

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,13 @@
 const { LinkedList } = require('./linked-list');
+
+const list = new LinkedList();
+
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+
+console.log(list.get(2));
+console.log(list.get(0));
+
+// list.printAll();
--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list.js
@@ -1,5 +1,48 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+  }
+
+  get(index) {
+    let current = this.head;
+    let i = 0;
+
+    while (i < index) {
+      current = current.next;
+      i++;
+    }
+
+    return current.data;
+  }
+
+  printAll() {
+    let current = this.head;
+
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
 
 module.exports = { Node, LinkedList };
