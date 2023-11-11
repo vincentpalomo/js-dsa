4bf49024acfb4442a13c8a4a9965a3a5aef78d93
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 09:48:04 -0600

    doubly linked list find pair sum solved

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,17 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
-function findPairSum() {}
+function findPairSum(nums, target) {
+  const seen = new DoublyLinkedList();
+
+  for (const num of nums) {
+    const difference = target - num;
+
+    if (seen.contains(difference)) return [difference, num];
+
+    seen.append(num);
+  }
+
+  return null;
+}
 
 module.exports = findPairSum;
