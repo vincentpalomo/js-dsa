f6b67e6ae7aeb062ca0dd41fde56f057703da1b2
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 13:53:15 -0600

    learned about breadth first traversal which goes each node at the current level

--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,32 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+  if (!root) return [];
+
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(root);
+
+  while (!queue.isEmpty()) {
+    const current = queue.dequeue();
+    result.push(current.data);
+
+    if (current.left) queue.enqueue(current.left);
+
+    if (current.right) queue.enqueue(current.right);
+  }
+
+  return result;
+}
 
 module.exports = {
   Node,
