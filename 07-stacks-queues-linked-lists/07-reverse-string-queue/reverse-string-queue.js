1a33ff592b46ea068966216239b948dfca5fc23c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-10 21:12:10 -0600

    completed reverse string

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,25 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  // create a queue variable
+  // create a reversed variable
+  // for loop the string
+  // add each letter to the queue
+  // dequeue each letter into a string
+  // return reversed
+  const characters = new Queue();
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    characters.enqueue(str[i]);
+  }
+  console.log(characters);
+
+  while (!characters.isEmpty()) {
+    reversed += characters.dequeue();
+  }
+
+  return reversed;
+};
 
 module.exports = reverseStringWithQueue;
