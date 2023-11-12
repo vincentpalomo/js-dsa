3e6f8b44ff67af2251c308af2ad51e72e8c4f4d8
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-11 18:06:14 -0600

    graph started

--- 08-binary-trees-graphs/12-graph-implementation/graph.js
+++ 08-binary-trees-graphs/12-graph-implementation/graph.js
@@ -1,4 +1,23 @@
-class Graph {}
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  printAdjacencyList() {
+    for (const vertex in this.adjacencyList) {
+      console.log(`${vertex} -> ${this.adjacencyList[vertex].join('')}`);
+    }
+  }
+}
 
 // Example Adjacency List
 // {
