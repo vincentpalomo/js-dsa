33c6561f060d3b7d8cf288cc3c76bae039872a57
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-12 13:36:06 -0600

    learned about selection sort

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,26 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  // outer loop to iterate thru each element
+  for (let i = 0; i < arr.length - 1; i++) {
+    // assume the current index is the minium
+    let minIndex = i;
+
+    // inner loop to find the index of the minimum element in the unsorted part of the arr
+    for (let j = i + 1; j < arr.length; j++) {
+      // compare the current element with the element at the minIndex
+      if (arr[j] < arr[minIndex]) {
+        // if a smaller element is found, update minIndex
+        minIndex = j;
+      }
+    }
+
+    // swap the found minimum element with the first element if they are different
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  // return sorted array
+  return arr;
+}
 
 module.exports = selectionSort;
