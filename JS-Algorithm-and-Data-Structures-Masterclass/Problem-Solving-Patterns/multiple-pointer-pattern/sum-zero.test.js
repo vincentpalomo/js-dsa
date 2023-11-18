8cdc3f6435a9d2e6125c95c01e4b3496f5fe6cae
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-17 20:37:12 -0600

    updated test file for sumzero

--- JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero.test.js
+++ JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero.test.js
@@ -1,1 +1,5 @@
 const sumZero = require('./sum-zero-jest');
+
+test('sumZero of [-3, -2, -1, 0, 1 ,2 ,3] should return [-3, 3]', () => {
+  expect(sumZero([-3, -2, -1, 0, 1, 2, 3])).toBe([-3, 3]);
+});
