512507e703c7592ec19bf6824cd85c80a115ca6e
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-17 22:09:41 -0600

    solved sum zero challenge using jest for testing

--- JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero-jest.js
+++ JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero-jest.js
@@ -2,7 +2,31 @@
 // the function should find the first pair where the sum is 0
 // return an array that inclues both values that sum to zero or undefined if pair doesnt exist
 
-const sumZero = () => {
+const sumZero = (arr) => {
+  // create a left pointer starting at 0
+  // create a right pointer starting at the end of input arr
+  // while left is less than right
+  // create a sum variable for left and right value
+  // if sum equals 0 return values left and right in an array
+  // else if sum is greater than 0
+  // decrement right
+  // else increment left
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
   return undefined;
 };
 
--- JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero.test.js
+++ JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/sum-zero.test.js
@@ -1,5 +1,13 @@
 const sumZero = require('./sum-zero-jest');
 
 test('sumZero of [-3, -2, -1, 0, 1 ,2 ,3] should return [-3, 3]', () => {
-  expect(sumZero([-3, -2, -1, 0, 1, 2, 3])).toBe([-3, 3]);
+  expect(sumZero([-3, -2, -1, 0, 1, 2, 3])).toStrictEqual([-3, 3]);
+});
+
+test('sumZero of [-4, -3, -2, -1, 0 , 1, 2, 5, 6] should return [-2, 2]', () => {
+  expect(sumZero([-4, -3, -2, -1, 0, 1, 2, 5, 6])).toStrictEqual([-2, 2]);
+});
+
+test('sumZero of [-3, -2, -1, 0, 4, 5, 6] should return undefined', () => {
+  expect(sumZero([-3, -2, -1, 0, 4, 5, 6])).toBeUndefined();
 });
