71e6fc61e3510d00d7f18cbcee6b627b6fea6cf9
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-19 10:56:42 -0600

    added another test to count unique

--- JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/count-unique.test.js
+++ JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/multiple-pointer-pattern/count-unique.test.js
@@ -15,3 +15,7 @@ test('The unique values in [-1, 0, 1, 1, 2, 3, 4, 5, 5, 6, 6, 7, 8 ,9] should eq
 test('The unique values in [-5, -4, -4, -3, -2, -2, -1, 0, 1, 2] should equal 8', () => {
   expect(countUniqueValues([-5, -4, -4, -3, -2, -2, -1, 0, 1, 2])).toBe(8);
 });
+
+test('The unique value in [1, 2, ,2 ,3] should equal 3', () => {
+  expect(countUniqueValues([1, 2, 2, 3])).toBe(3);
+});
