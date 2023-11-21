486140e1d1795c80bd4e8ba3848c6835643ada7c
Vincent Palomo <vincentpalomo@yahoo.com>
2023-11-20 18:59:20 -0600

    added console logs to better understand the O(n) solution of maxsubarraysum

--- JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/sliding-window-pattern/maxSubarraySum.js
+++ JS-Algorithm-and-Data-Structures-Masterclass/Problem-Solving-Patterns/sliding-window-pattern/maxSubarraySum.js
@@ -7,24 +7,43 @@ const maxSubarraySum = (arr, num) => {
   // update the tempsum using the sliding window technique
   // update the maxsum with the maximum of current maxsum and tempsum (Math.max)
   // return maxsum
+  // let maxSum = 0;
+  // let tempSum = 0;
+  // if (arr.length < num) return null;
+  // for (let i = 0; i < num; i++) {
+  //   maxSum += arr[i];
+  // }
+  // tempSum = maxSum;
+  // for (let i = num; i < arr.length; i++) {
+  //   tempSum = tempSum - arr[i - num] + arr[i];
+  //   // console.log(`Updated temp sum: ${tempSum} (removed ${arr[i - num]}, added ${arr[i]})`);
+  //   maxSum = Math.max(maxSum, tempSum);
+  // }
+  // return maxSum;
+
   let maxSum = 0;
   let tempSum = 0;
 
-  if (arr.length < num) return null;
+  if (num > arr.length) return null;
 
   for (let i = 0; i < num; i++) {
     maxSum += arr[i];
   }
 
+  console.log(maxSum);
+
   tempSum = maxSum;
+
+  console.log(tempSum);
 
   for (let i = num; i < arr.length; i++) {
     tempSum = tempSum - arr[i - num] + arr[i];
-    // console.log(`Updated temp sum: ${tempSum} (removed ${arr[i - num]}, added ${arr[i]})`);
+    console.log(tempSum);
     maxSum = Math.max(maxSum, tempSum);
   }
-
-  return maxSum;
+  console.log(maxSum);
 };
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 3);
 
 module.exports = maxSubarraySum;
